e045b3945b9f6cfa890b551cfaabb4dac36890cc
Muhammad Mobeen Ghaffar <61686789+Mobeen22-creator@users.noreply.github.com>
2022-04-01 05:34:26 +0500

    Update example.py

--- example.py
+++ example.py
@@ -4,4 +4,4 @@ password="" # Password to encrypt
 key="" # Scrape it from signup page
 randomNum=""# Scrape it from signup page
 SKI="" # Scrape it from signup page
-value = ctx.call('Encrypt',"", "","newpwd",password,key,randomNum,SKI)
+encrpyted_password = ctx.call('Encrypt',"", "","newpwd",password,key,randomNum,SKI)
